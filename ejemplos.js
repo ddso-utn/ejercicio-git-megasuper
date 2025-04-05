840c295c753ebdfb4cb0b12f6c210853a6d8513e
Juan Pablo Castiglione <juanpablocastiglione01@gmail.com>
2025-04-05 08:42:05 -0300

    ejemplo

--- ejemplos.js
+++ ejemplos.js
@@ -1,5 +1,5 @@
 const { Carrito } = require("./domain/Carrito");
-const { DescuentoFijo } = require("./domain/Descuento");
+const { DescuentoFijo, DescuentoPorCantidad, DescuentoPorcentual } = require("./domain/Descuento");
 const { ItemCarrito } = require("./domain/ItemCarrito");
 const { Producto } = require("./domain/Producto");
 
@@ -7,6 +7,8 @@ const p1 = new Producto("Coca", 10, 2)
 const carrito = new Carrito()
 const item = new ItemCarrito(p1, 2)
 item.agregarDescuento(new DescuentoFijo(5))
+item.agregarDescuento(new DescuentoPorCantidad(2, 50))
+item.agregarDescuento(new DescuentoPorcentual(10))
 carrito.agregarItem(item)
 
 console.log(carrito.precioTotal())
